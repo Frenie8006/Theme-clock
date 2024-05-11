32ee77de7483d2a18f2e3d47abc9c770fe8bfe07
frenie8006 <seonitabatucan57@gmail.com>
2024-05-11 19:57:20 +0800

    Dark mode and clock functionality

--- script.js
+++ script.js
@@ -1,5 +1,107 @@
 'use strict';
 
-const toggle = document.querySelector('.toggle');
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
 
-toggle.addEventListener('click', () => document.body.classList.toggle('dark'));
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'July',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+/////////////////////////////////////
+
+// Function to toggle dark mode
+const toggleDarkMode = () => {
+  const body = document.body;
+  const toggleButton = document.querySelector('.toggle');
+
+  toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    const mode = body.classList.contains('dark') ? 'Light Mode' : 'Dark Mode';
+    toggleButton.textContent = mode;
+  });
+};
+
+// Function to rendering time clock
+const setTime = function () {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const hoursClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeEl.textContent = `${hoursClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="date-number">${date}</span>`;
+};
+
+// Using this to function the needles of the time clock
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers?newreg=8d36ca2bc0104c839e381fec3cc467ff
+const scale = function (number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
+
+// Initialize the script
+const init = () => {
+  toggleDarkMode();
+  setTime();
+
+  // Show animation effects everytime second is rendering using setInterval
+  setInterval(setTime, 1000);
+};
+
+// Run the initialization function when the DOM is ready
+document.addEventListener('DOMContentLoaded', init);
